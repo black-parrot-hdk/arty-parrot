55912f09c201a13c48566f430e49dd6f276c96e1
Mark Wyse <muwyse@gmail.com>
2021-03-13 10:50:57 -0800

    improve uart.py PC Host script

--- py/uart.py
+++ py/uart.py
@@ -4,39 +4,53 @@ import serial
 import argparse
 import signal
 import atexit
+import time
 
-parser = argparse.ArgumentParser(description='UART Driver')
-parser.add_argument('-p', '--port', dest='port', type=str, default='/dev/ttyS4',
-                    help='Serial port (full path)')
-parser.add_argument('-b', '--baud', dest='baud', type=int, default=9600,
-                    help='Baud Rate (bits per second)')
-parser.add_argument('-d', '--data-bits', dest='bits', default=8, const=8, type=int,
-                    nargs='?', choices=[5, 6, 7, 8],
-                    help='Data bits [5, 6, 7, 8]')
-parser.add_argument('-s', '--stop-bits', dest='stopbits', default=1, const=1, type=int,
-                    nargs='?', choices=[1, 2],
-                    help='Stop bits [1, 2]')
-parser.add_argument('--parity', dest='parity', default='none', const='none',
-                    nargs='?', choices=['none', 'even', 'odd'],
-                    help='Parity [none, even, odd]')
-parser.add_argument('-f', '--file', dest='infile', default=None, type=str,
-                    help='Input file')
-parser.add_argument('-m', '--mode', dest='mode', default='input-ch', const='input-ch',
-                    nargs='?', choices=['binary', 'hex', 'nbf', 'input-ch', 'input-hex'],
-                    help='Input file mode [binary, hex, nbf, input-ch, input-hex]. input- modes read from stdin')
-parser.add_argument('-n', '--nbf-bits', dest='nbf', default=112, type=int,
-                    help='Number of bits per NBF command (opcode + address + data)')
-
-# serial port
+## Global variables
+# Serial Port
 sp = None
 
+## Program Arguments
 def getArgs():
+  parser = argparse.ArgumentParser(description='UART Driver')
+  # UART parameters
+  parser.add_argument('-p', '--port', dest='port', type=str, default='/dev/ttyS4',
+                      help='Serial port (full path)')
+  parser.add_argument('-b', '--baud', dest='baud', type=int, default=9600,
+                      help='Baud Rate (bits per second)')
+  parser.add_argument('-d', '--data-bits', dest='bits', default=8, const=8, type=int,
+                      nargs='?', choices=[5, 6, 7, 8],
+                      help='Data bits [5, 6, 7, 8]')
+  parser.add_argument('-s', '--stop-bits', dest='stopbits', default=1, const=1, type=int,
+                      nargs='?', choices=[1, 2],
+                      help='Stop bits [1, 2]')
+  parser.add_argument('--parity', dest='parity', default='none', const='none',
+                      nargs='?', choices=['none', 'even', 'odd'],
+                      help='Parity [none, even, odd]')
+  parser.add_argument('-t', '--timeout', dest='timeout', default=1.0, type=float,
+                      help='Read timeout')
+  # Mode
+  parser.add_argument('-m', '--mode', dest='mode', default='char', const='char',
+                      nargs='?', choices=['nbf', 'char', 'hex'],
+                      help='Input file mode [nbf, char, hex]. char and hex modes read from stdin')
+  parser.add_argument('-f', '--file', dest='infile', default=None, type=str,
+                      help='Input file')
+  # NBF mode parameters
+  parser.add_argument('--nbf-op-bytes', dest='nbf_op_bytes', default=1, type=int,
+                      help='Number of bytes per NBF opcode')
+  parser.add_argument('--nbf-addr-bytes', dest='nbf_addr_bytes', default=5, type=int,
+                      help='Number of bytes per NBF address')
+  parser.add_argument('--nbf-data-bytes', dest='nbf_data_bytes', default=8, type=int,
+                      help='Number of bytes per NBF data')
+  parser.add_argument('--nbf-listen-timeout', dest='nbf_listen_timeout', default=8, type=int,
+                      help='Seconds to wait for NBF packets from FPGA before prompting user')
   return parser.parse_args()
 
 def openFile(infile, mode):
   fp = os.path.abspath(os.path.realpath(infile))
   return open(fp, mode)
 
+## Serial Port Functions
 def openSerial(args):
   bytesize = serial.EIGHTBITS
   if (args.bits == 5):
@@ -56,12 +70,14 @@ def openSerial(args):
   if (args.stopbits == 2):
     stopbits = serial.STOPBITS_TWO
 
+  timeout = args.timeout
+  if (timeout < 0):
+    timeout = None
+
   return serial.Serial(port=args.port, baudrate=args.baud, bytesize=bytesize,
-                       parity=parity, stopbits=stopbits)
+                       parity=parity, stopbits=stopbits, timeout=timeout)
 
-def closeSerial(serialPort):
-  serialPort.close()
-
+## Formatting Functions
 def encodeString(string):
   return string.encode('utf-8')
 
@@ -73,58 +89,19 @@ def hexStringToBytes(string):
     b = None
   return b
 
-# Exit and Signal handlers
+## Exit and Signal handlers
 def exitHandler():
-  print("goodbye!")
   if not sp is None and sp.is_open:
     print("closing serial port: {0}".format(sp.name))
     sp.close()
 
-def signalHandler(sig, frame):
-  print("signal handler")
+def sigIntHandler(sig, frame):
   if not sp is None and sp.is_open:
     print("closing serial port: {0}".format(sp.name))
     sp.close()
   sys.exit(1)
 
-
-# modes
-def runBinary(args):
-  pass
-
-def runHex(args):
-  pass
-
-def readNBF():
-  opcode = sp.read(1).hex()
-  addr = sp.read(5).hex()
-  data = sp.read(8).hex()
-  print('NBF: {0}_{1}_{2}'.format(opcode, addr, data))
-
-def nbfHasResponse(nbf_bytes):
-  resp_ops = [254, 255, 2, 3, 18, 19]
-  return (nbf_bytes[0] in resp_ops)
-
-def runNBF(args):
-  # process input file as BlackParrot NBF format
-  # each line of nbf file is hex characters
-  # underscores may be present to separate hex
-  try:
-    bytes_written = 0
-    with openFile(args.infile, 'r') as f:
-      for line in f:
-        line = line.strip().replace('_','')
-        line_bytes = hexStringToBytes(line)
-        sp.write(line_bytes)
-        bytes_written += len(line_bytes)
-        if (nbfHasResponse(line_bytes)):
-          readNBF()
-    print('wrote {0} bytes from nbf'.format(bytes_written))
-  except:
-    print('failed to transfer nbf file')
-    if not sp is None and sp.is_open:
-      sp.close()
-
+## Interactive Modes
 def interactiveHex(args):
   user_input = None
   while (True):
@@ -146,24 +123,116 @@ def interactiveCh(args):
     print('sent {0} bytes'.format(user_input_length))
     print('readback: {0}'.format(sp.read(user_input_length)))
 
+## NBF Mode
+
+# encode 'op_addr_data' hex nbf string to bytes
+def encodeNBF(string):
+  return hexStringToBytes(string.strip().replace('_',''))
+
+# decode (op, addr, data) bytes to 'op_addr_data' hex nbf string
+def decodeNBF(op_bytes, addr_bytes, data_bytes):
+  return '{0}_{1}_{2}'.format(op_bytes.hex(), addr_bytes.hex(), data_bytes.hex())
+
+# split 'op_addr_data' hex string into (op, addr, data) hex string tuple
+def splitNBF(args, nbf):
+  nbf_parts = nbf.strip().split('_')
+  opcode = nbf_parts[0]
+  addr = nbf_parts[1]
+  data = nbf_parts[2]
+  return (opcode, addr, data)
+
+# determine if NBF command has response
+def nbfHasResponse(opcode_hex):
+  # BP NBF opcodes requiring response from FPGA
+  resp_ops = ['02', '03', '12', '13', 'fe', 'ff']
+  return (opcode_hex in resp_ops)
+
+# read NBF packet from serial port, return (op, addr, data) hex strings
+def readNBF(args):
+  opcode = sp.read(args.nbf_op_bytes)
+  addr = sp.read(args.nbf_addr_bytes)
+  data = sp.read(args.nbf_data_bytes)
+  if (len(opcode) != args.nbf_op_bytes) or (len(addr) != args.nbf_addr_bytes) or (len(data) != args.nbf_data_bytes):
+    print('Failed to recevie full NBF packet')
+    return (None, None, None)
+  return (opcode.hex(), addr.hex(), data.hex())
+
+# write NBF packet to serial port, return bytes written
+# also waits for response if command has a response
+def writeNBF(args, nbf_hex):
+  try:
+    bytes_written = 0
+    (opcode, addr, data) = splitNBF(args, nbf_hex)
+    nbf_bytes = encodeNBF(nbf_hex)
+    bytes_written = len(nbf_bytes)
+    print('SEND: {0}'.format(nbf_hex))
+    sp.write(nbf_bytes)
+    if (nbfHasResponse(opcode)):
+      (opcode_in, addr_in, data_in) = readNBF(args)
+      print('REPLY: {0}_{1}_{2}'.format(opcode_in, addr_in, data_in))
+    return bytes_written
+  except:
+    print('failed to transfer nbf file')
+    return 0
+
+# transfer NBF file, command by command
+def sendNBF(args):
+  # process input file as BlackParrot NBF format
+  # each line of nbf file is hex characters
+  # underscores may be present to separate hex
+  try:
+    bytes_written = 0
+    with openFile(os.path.abspath(args.infile), 'r') as f:
+      for nbf in f:
+        bytes_written += writeNBF(args, nbf)
+    print('wrote {0} bytes'.format(bytes_written))
+  except:
+    print('failed to transfer nbf file...closing')
+    if not sp is None and sp.is_open:
+      sp.close()
+
+# listen on serial port for NBF packets
+def listenNBF(args):
+  timeout_cnt = 0
+  # loop until user says stop
+  while(True):
+    # check if serial port has bytes, and try to process if it does
+    # some bytes received, read them from serial port
+    if sp.in_waiting > 0:
+      (opcode_in, addr_in, data_in) = readNBF(args)
+      if (opcode_in is None):
+        return
+      print('REPLY: {0}_{1}_{2}'.format(opcode_in, addr_in, data_in))
+      timeout_cnt = 0
+    # no bytes received, increment timeout counter
+    # sleep for a second
+    else:
+      timeout_cnt += 1
+      sleep(1)
+
+    if timeout_cnt > args.nbf_listen_timeout:
+      resp = input('$ Continue executing (y/n)? ')
+      if (resp.lower() in ['n', 'no']):
+        return
+      else:
+        timeout_cnt = 0
+
+
+# NBF mode entry
+def runNBF(args):
+  sendNBF(args)
+
+## Main
 if __name__ == '__main__':
   atexit.register(exitHandler)
-  signal.signal(signal.SIGINT, signalHandler)
+  signal.signal(signal.SIGINT, sigIntHandler)
   args = getArgs()
   try:
     sp = openSerial(args)
-    if args.mode == 'input-ch':
+    if args.mode == 'char':
       interactiveCh(args)
-    elif args.mode == 'input-hex':
-      interactiveHex(args)
-    elif args.mode == 'binary':
-      f = openFile(args.infile, 'rb')
-      f.close()
-      pass
     elif args.mode == 'hex':
-      f = openFile(args.infile, 'r')
-      f.close()
-      pass
+      interactiveHex(args)
     elif args.mode == 'nbf':
       runNBF(args)
   except:
